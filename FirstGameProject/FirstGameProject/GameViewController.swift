0bb45ba275fb7278a2174f5e44631ae1b84f9563
Kaique Damato <kaique_pantosi@hotmail.com>
2015-05-06 09:13:37 -0300

    commit inicial

---
+++ FirstGameProject/FirstGameProject/GameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameViewController.swift
+//  FirstGameProject
+//
+//  Created by Kaique Damato on 5/4/15.
+//  Copyright (c) 2015 Kaique Damato. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
