69181a15276dc6921f4ccf6f65e3188a1a259a16
Kaique Damato <kaique_pantosi@hotmail.com>
2015-05-06 09:10:26 -0300

    commit inicial

---
+++ SecondGameProject/SecondGameProject/GameOverScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameOverScene.swift
+//  SecondGameProject
+//
+//  Created by Kaique Damato on 5/6/15.
+//  Copyright (c) 2015 Kaique Damato. All rights reserved.
+//
+
+import SpriteKit
+
+let GameOverLabelCategoryName = "gameOverLabel"
+
+class GameOverScene: SKScene {
+    var gameWon : Bool = false {
+        // 1.
+        didSet {
+            let gameOverLabel = childNodeWithName(GameOverLabelCategoryName) as! SKLabelNode
+            gameOverLabel.text = gameWon ? "Game Won" : "Game Over"
+        }
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let view = view {
+            // 2.
+            let gameScene = GameScene.unarchiveFromFile("GameScene") as! GameScene
+            view.presentScene(gameScene)
+        }
+    }
+}
